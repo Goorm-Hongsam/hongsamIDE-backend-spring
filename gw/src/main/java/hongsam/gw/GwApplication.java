3201ff175b10a7bbdc234a7620fc6c7fcaf98016
woo <ldw28517@naver.com>
2024-02-05 15:03:48 +0900

    first commit

---
+++ gw/src/main/java/hongsam/gw/GwApplication.java
@@ -0,0 +1,13 @@
+package hongsam.gw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GwApplication.class, args);
+	}
+
+}
---
+++ gw/src/test/java/hongsam/gw/GwApplicationTests.java
@@ -0,0 +1,13 @@
+package hongsam.gw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
